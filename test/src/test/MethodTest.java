002ebe5e777f0bab29da8e74b4f3372c83689e5c
DESKTOP-DLCB8GM\505 <lkw8342@gmail.com>
2022-05-25 17:27:03 +0900

    Revert "0526 java study"
    This reverts commit 44c1324fc1c0dc920f862cc538b206129bee7642.

--- test/src/test/MethodTest.java
+++ test/src/test/MethodTest.java
@@ -6,13 +6,14 @@ public class MethodTest {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int x;
 		
 		while(true) {
-			System.out.print("구구셈 몇단 : ");
-			int dan=sc.nextInt();
-			if(!(dan==0 ||dan==1)) {
+			System.out.print("입력 : ");
+			x=sc.nextInt();
+			if((x>=2)&&(x<=9)) {
 				for(int i=1;i<=9;i++) 
-					System.out.println(dan+" * "+i+" = "+dan*i);
+					System.out.println(x+" * "+i+" = "+x*i);
 			}
 			else { 
 				System.out.println("프로그램 종료");
--- work13/src/w13_1/Human.java
+++
@@ -1,7 +0,0 @@
-package w13_1;
-
-public class Human {
-	String name;
-	int height;
-	int weight;
-}
